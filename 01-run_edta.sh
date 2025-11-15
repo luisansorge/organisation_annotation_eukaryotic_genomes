8fa0bc966c9dc6ef7cb3d45f8c152a8db12c8dc6
Luis Ansorge <lansorge@login8.hpc.binf.unibe.ch>
2025-11-15 16:10:16 +0100

    edited EDTA script

--- 01-run_edta.sh
+++ 01-run_edta.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 #SBATCH --job-name=te_annot
-#SBATCH --partition=pibu_el8 # IBU cluster partition
-#SBATCH --cpus-per-task=20 # e.g. 20
-#SBATCH --mem=64 # e.g. 64G or 200G
-#SBATCH --time=2-00:00 # format D-HH:MM, e.g. 2-00:00
+#SBATCH --partition=pibu_el8
+#SBATCH --cpus-per-task=20 
+#SBATCH --mem=200G 
+#SBATCH --time=2-00:00 
 
 # User-editable variables
 WORKDIR=/data/users/lansorge/organisation_annotation
+# Path to EDTA singularity container (v2.2)
 CONTAINER=/data/courses/assembly-annotation-course/CDS_annotation/containers/EDTA2.2.sif
+# Input genome assembly (Hifiasm) in FASTA format
 INPUT_FASTA=/data/users/lansorge/assembly_annotation_course/hifiasm_assembly/Abd-0.asm.bp.p_ctg.fa
 OUTDIR=$WORKDIR/results/EDTA_annotation
 
@@ -15,12 +17,12 @@ mkdir -p "$OUTDIR"
 cd "$OUTDIR"
 
 apptainer run --bind /data \
-    "$CONTAINER" \
+    "$CONTAINER" \  
     EDTA.pl \
     --genome $INPUT_FASTA \
-    --species others \
-    --step all \
-    --sensitive 1 \
-    --cds "/data/courses/assembly-annotation-course/CDS_annotation/data/TAIR10_cds_20110103_representative_gene_model_updated" \
-    --anno 1 \
+    --species others \       # Generic TE library (not species-specific)
+    --step all \        # Run all annotation steps (LTR, TIR, Helitron, etc.)
+    --sensitive 1 \      # Sensitive mode for better TE detection
+    --cds "/data/courses/assembly-annotation-course/CDS_annotation/data/TAIR10_cds_20110103_representative_gene_model_updated" \        # Provide CDS sequences to avoid misclassifying genes as TEs
+    --anno 1 \      # Genome-wide TE annotation (intact and fragmented TEs)
     --threads 20
